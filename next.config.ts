ada625e226e5c6543aecfe6ce353c7d4325275fa
Jonah Boling <56607167+jonahrb@users.noreply.github.com>
2026-05-06 15:46:57 -0500

    Deploy Next.js to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/my-next-app',
+  assetPrefix: '/my-next-app/',
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
