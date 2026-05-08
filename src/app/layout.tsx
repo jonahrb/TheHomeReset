15499f7e09e4b469e3fc68aaf794c870b5f5c16e
Jonah Boling <56607167+jonahrb@users.noreply.github.com>
2026-05-08 14:33:50 -0500

    add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "The Home Reset | Professional Home Cleaning",
@@ -23,6 +24,7 @@ export default function RootLayout({
         <main className="flex-1">{children}</main>
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
